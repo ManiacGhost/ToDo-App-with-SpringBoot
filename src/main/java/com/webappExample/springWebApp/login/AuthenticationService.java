1dc1587560467d8f3c32c02ad73befeee5959822
ManiacGhost <harshpandey6754@gmail.com>
2024-07-29 15:02:57 +0530

    hardcoded authentication and models

---
+++ src/main/java/com/webappExample/springWebApp/login/AuthenticationService.java
@@ -0,0 +1,15 @@
+package com.webappExample.springWebApp.login;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+	
+	public boolean authenticate(String username, String password) {
+		
+		boolean isValidUserName = username.equalsIgnoreCase("harshpandey	");
+		boolean isValidPassword = password.equalsIgnoreCase("password");
+		
+		return isValidUserName && isValidPassword;
+	}
+}
